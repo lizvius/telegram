9cbc249a489a9da6d0e406c987b9b52240115d5e
AzurLize <93086841+lizvius@users.noreply.github.com>
2026-07-20 16:02:40 +0800

    build: optimize build configuration and CI
    - Remove manual debug keystore management.
    - Update JVM and worker settings for build stability.
    - Switch Kotlin compiler execution to out-of-process.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,12 +30,6 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
   }
 
   buildTypes {
@@ -45,7 +39,7 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+    debug { }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
